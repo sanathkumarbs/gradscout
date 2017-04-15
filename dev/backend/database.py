b37cf2277cdcdc5dc54e3eed0b43d817101af891
Sanath Kumar <b.s.sanathkumar@gmail.com>
2017-04-15 15:03:26 -0700

    Fixed database speed, code review changes

--- dev/backend/database.py
+++ dev/backend/database.py
@@ -11,6 +11,20 @@ class Firebase(object):
         """Initialize Firebase Object for Database Access."""
         self.firebase = firebase.FirebaseApplication(
             'https://gradscout-40fed.firebaseio.com/', None)
+        self.data = self._get_data()
+
+    def _get_data(self):
+        """Get complete data for quicker cached access.
+
+        Args:
+            program_id (int): Unique ID for Program
+
+        Returns:
+            program_dict (dict): Python Dict of Complete Program Details
+        """
+        endpoint = "/programs/"
+        result = self.firebase.get(endpoint, None)
+        return result
 
     def get_detailed_program(self, program_id):
         """Get the Detailed Program Details for a given Program ID.
@@ -21,8 +35,12 @@ class Firebase(object):
         Returns:
             program_dict (dict): Python Dict of Complete Program Details
         """
-        endpoint = "/programs/" + str(program_id)
-        result = self.firebase.get(endpoint, None)
+        result = None
+        try:
+            result = self.data[int(program_id)]
+        except KeyError:
+            endpoint = "/programs/" + str(program_id)
+            result = self.firebase.get(endpoint, None)
         return result
 
     def get_program_count(self):
@@ -31,9 +49,13 @@ class Firebase(object):
         Returns:
             count (int): Total count of the programs in the database
         """
-        endpoint = "/programs/"
-        result = self.firebase.get(endpoint, None)
-        count = len(result)
+        count = None
+        try:
+            count = len(self.data)
+        except KeyError:
+            endpoint = "/programs/"
+            result = self.firebase.get(endpoint, None)
+            count = len(result)
         return count
 
     def get_program_research(self, program_id):
@@ -45,8 +67,12 @@ class Firebase(object):
         Returns:
             research_dict (dict): Python Dict of Research Details
         """
-        endpoint = "/programs/" + str(program_id) + '/research'
-        result = self.firebase.get(endpoint, None)
+        result = None
+        try:
+            result = self.data[int(program_id)].get('research')
+        except KeyError:
+            endpoint = "/programs/" + str(program_id) + '/research'
+            result = self.firebase.get(endpoint, None)
         return result
 
     def get_program_admission_rate(self, program_id):
@@ -58,8 +84,12 @@ class Firebase(object):
         Returns:
             admission_rate (int): Admission Rate in Percentage
         """
-        endpoint = "/programs/" + str(program_id) + '/admission_rate'
-        result = self.firebase.get(endpoint, None)
+        result = None
+        try:
+            result = self.data[int(program_id)].get('admission_rate')
+        except KeyError:
+            endpoint = "/programs/" + str(program_id) + '/admission_rate'
+            result = self.firebase.get(endpoint, None)
         return result
 
     def get_program_fees(self, program_id):
@@ -71,8 +101,12 @@ class Firebase(object):
         Returns:
             fees_dict (dict): Python Dict of Program Fees
         """
-        endpoint = "/programs/" + str(program_id) + '/fees'
-        result = self.firebase.get(endpoint, None)
+        result = None
+        try:
+            result = self.data[int(program_id)].get('fees')
+        except KeyError:
+            endpoint = "/programs/" + str(program_id) + '/fees'
+            result = self.firebase.get(endpoint, None)
         return result
 
     def get_program_acad(self, program_id):
@@ -84,8 +118,12 @@ class Firebase(object):
         Returns:
             acad_dict (dict): Python Dict of Academic Requirement Details
         """
-        endpoint = "/programs/" + str(program_id) + '/academic_requirements'
-        result = self.firebase.get(endpoint, None)
+        result = None
+        try:
+            result = self.data[int(program_id)].get('academic_requirements')
+        except KeyError:
+            endpoint = "/programs/" + str(program_id) + '/academic_requirements'
+            result = self.firebase.get(endpoint, None)
         return result
 
     def get_program_living(self, program_id):
@@ -97,8 +135,12 @@ class Firebase(object):
         Returns:
             program_dict (dict): Python Dict of Living Expense Details
         """
-        endpoint = "/programs/" + str(program_id) + '/living_expenditure'
-        result = self.firebase.get(endpoint, None)
+        result = None
+        try:
+            result = self.data[int(program_id)].get('living_expenditure')
+        except KeyError:
+            endpoint = "/programs/" + str(program_id) + '/living_expenditure'
+            result = self.firebase.get(endpoint, None)
         return result
 
     def get_program_location(self, program_id):
@@ -107,8 +149,12 @@ class Firebase(object):
         Returns:
             location_dict (dict): Python Dict of Location Details
         """
-        endpoint = "/programs/" + str(program_id) + '/location'
-        result = self.firebase.get(endpoint, None)
+        result = None
+        try:
+            result = self.data[int(program_id)].get('location')
+        except KeyError:
+            endpoint = "/programs/" + str(program_id) + '/location'
+            result = self.firebase.get(endpoint, None)
         return result
 
     def get_program_ownership(self, program_id):
@@ -120,8 +166,12 @@ class Firebase(object):
         Returns:
             ownership_dict (dict): Python Dict of Ownership Details
         """
-        endpoint = "/programs/" + str(program_id) + '/ownership'
-        result = self.firebase.get(endpoint, None)
+        result = None
+        try:
+            result = self.data[int(program_id)].get('ownership')
+        except KeyError:
+            endpoint = "/programs/" + str(program_id) + '/ownership'
+            result = self.firebase.get(endpoint, None)
         return result
 
     def get_program_details(self, program_id):
@@ -133,8 +183,12 @@ class Firebase(object):
         Returns:
             program_dict (dict): Python Dict of Program Details
         """
-        endpoint = "/programs/" + str(program_id) + '/program'
-        result = self.firebase.get(endpoint, None)
+        result = None
+        try:
+            result = self.data[int(program_id)].get('program')
+        except KeyError:
+            endpoint = "/programs/" + str(program_id) + '/program'
+            result = self.firebase.get(endpoint, None)
         return result
 
     def get_program_university(self, program_id):
@@ -146,8 +200,12 @@ class Firebase(object):
         Returns:
             program_dict (dict): Python Dict of University Details
         """
-        endpoint = "/programs/" + str(program_id) + '/university'
-        result = self.firebase.get(endpoint, None)
+        result = None
+        try:
+            result = self.data[int(program_id)].get('university')
+        except KeyError:
+            endpoint = "/programs/" + str(program_id) + '/university'
+            result = self.firebase.get(endpoint, None)
         return result
 
     def get_program_rank(self, program_id):
@@ -159,6 +217,10 @@ class Firebase(object):
         Returns:
             program_dict (dict): Python Dict of Rank Details
         """
-        endpoint = "/programs/" + str(program_id) + '/rank'
-        result = self.firebase.get(endpoint, None)
+        result = None
+        try:
+            result = self.data[int(program_id)].get('rank')
+        except KeyError:
+            endpoint = "/programs/" + str(program_id) + '/rank'
+            result = self.firebase.get(endpoint, None)
         return result
